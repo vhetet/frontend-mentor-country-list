66ee84c7a37d993b2cf78d879cf6b5f833903575
vhetet <vh1030478@gmail.com>
2021-11-08 21:46:55 -0600

    configure fonts and icons

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.scss';
+import "./App.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCoffee } from '@fortawesome/free-solid-svg-icons'
+
 
 function App() {
   return (
     <div className="App">
-      
+      <FontAwesomeIcon icon={faCoffee} />
+      <p>this is a test</p>
     </div>
   );
 }
