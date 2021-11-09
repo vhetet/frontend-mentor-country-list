7b014379fa275339f06552bf4dffe19e5daaf0e3
vhetet <vh1030478@gmail.com>
2021-11-08 22:15:10 -0600

    add html strucutre for home page

--- src/App.js
+++ src/App.js
@@ -1,13 +1,36 @@
 import "./App.scss";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faCoffee } from '@fortawesome/free-solid-svg-icons'
-
+import { faMoon, faSearch } from "@fortawesome/free-solid-svg-icons";
 
 function App() {
   return (
     <div className="App">
-      <FontAwesomeIcon icon={faCoffee} />
-      <p>this is a test</p>
+      <div className="header">
+        <h4>Where in the world?</h4>
+        <div className="theme-switch">
+          <FontAwesomeIcon icon={faMoon} />
+          <span>Dark Mode</span>
+        </div>
+      </div>
+      <div className="search">
+        <FontAwesomeIcon icon={faSearch} />
+        <input type="text" placeholder="Search for a country" />
+      </div>
+      <div className="filter">
+        <input type="button" value="Filter by Region" />
+        {/* <input type="button">Filter by Region</input> */}
+      </div>
+      <div className="country-list">
+        <div className="card">
+          <div className="flag"></div>
+          <div className="card-details">
+            <h3 className="name">Germany</h3>
+            <p>Population: <span>81,770,900</span></p>
+            <p>Region: <span>Europe</span></p>
+            <p>Capital: <span>Berlin</span></p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
